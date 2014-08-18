ae0c69f85b4f8dc9bbfba9f0dd8510f00e784073
Pekka Huhtala <pekka.huhtala@gmail.com>
2014-08-18 15:17:39 +0300

    Initial commit

---
+++ number-comma.js
@@ -0,0 +1,37 @@
+﻿/*
+ * This DataTables plugin sorts a numeric column with price link.
+ * Cells can include a link and a EUR sign.
+ *
+ *  @name Date (dd.mm.YYYY)
+ *  @summary Sort date or date range in the format `dd.mm.YYYY `.
+ *  @author [Pekka Huhtala]
+ *
+ *  @example
+ *    $('#example').dataTable( {
+ *       columnDefs: [
+ *         { type: 'numeric-comma', targets: 0 },
+ *       ]
+ *    } );
+ */
+
+
+jQuery.fn.dataTableExt.oSort['numeric-comma-pre'] = function (a, b) {
+    a = $(a).text().replace("€", "");
+    a = $.trim(a);
+    var x = (a == "-") ? 0 : a.replace(/,/, ".");
+    x = parseFloat(x);
+    return x;
+
+}
+jQuery.fn.dataTableExt.oSort['numeric-comma-asc'] = function (a, b) {
+    x = parseFloat(a);
+    y = parseFloat(b);
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort['numeric-comma-desc'] = function (a, b) {
+    x = parseFloat(a);
+    y = parseFloat(b);
+    return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+};
+
